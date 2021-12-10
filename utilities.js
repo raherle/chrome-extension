147e9a00eba7b34b036080e94361c339760989a6
Rahel Aumann <rahel.aumann@klosebrothers.de>
2021-12-10 10:57:10 +0100

    press button

--- utilities.js
+++ utilities.js
@@ -1,10 +1,18 @@
 /**
  * Gets the desired element on the client page and clicks on it
  */
-function goToActivityTab() {
-    var activityTab = document.getElementsByClassName("Tg7LZd")[0];
+async function goToActivityTab() {
+    const buttons = document.getElementsByTagName('button')
+    Array.prototype.slice.call( buttons, 0 ).filter(btn => btn.innerText === 'Eintragen')[0].click()
 
-    activityTab.click();
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    await sleep(2000);
+
+    Array.prototype.slice.call(document.getElementsByTagName('input'), 0)
+        .filter(btn => btn.ariaLabel === 'Titel hinzufügen')[0].value = "Rahel HO";
 }
 
 goToActivityTab();
