32a5643e898f500f1794653c1ea4622eff54a9f7
Rahel Aumann <rahel.aumann@klosebrothers.de>
2021-12-10 11:08:49 +0100

    check whole day checkbox

--- utilities.js
+++ utilities.js
@@ -13,6 +13,9 @@ async function goToActivityTab() {
 
     Array.prototype.slice.call(document.getElementsByTagName('input'), 0)
         .filter(btn => btn.ariaLabel === 'Titel hinzufügen')[0].value = "Rahel HO";
+
+    Array.prototype.slice.call(document.getElementsByTagName('div'), 0)
+        .filter(btn => btn.ariaLabel === 'Ganztägig')[0].click();
 }
 
 goToActivityTab();
