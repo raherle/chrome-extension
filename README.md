fbbee7436ae84072686e1be3c042fd2102cadd41
Rahel Aumann <rahel.aumann@klosebrothers.de>
2021-12-10 08:45:10 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # chrome-extension
+
+###How to use
+
+- Go to `chrome://extensions/`
+- Press "Load unpacked" and choose "chrome-extension" folder
+- Pin in to quick access inside chrome
+- Open extension by pressing the symbol
---
+++ utilities.js
@@ -0,0 +1,10 @@
+/**
+ * Gets the desired element on the client page and clicks on it
+ */
+function goToActivityTab() {
+    var activityTab = document.getElementsByClassName("Tg7LZd")[0];
+
+    activityTab.click();
+}
+
+goToActivityTab();
